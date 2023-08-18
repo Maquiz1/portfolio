3222bb7dc38ef359c5d687464a8072953f19ebcb
Maquiz1 <manquiz92@gmail.com>
2023-08-18 04:21:49 +0300

    first commit3

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 
 def blog_index(request):
@@ -25,10 +26,24 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+    # RENDER TO USER
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form,  #ADD A COMMENT FIELD TO A USER TO SEE
     }
 
     return render(request, "blog_detail.html", context)
